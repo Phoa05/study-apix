bf0749d3d4c6a2508950be122296fdfa5f46fc67
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-10 22:42:54 -0300

    feat: criando classe de produto

--- src/main/java/com/github/phoa05/study_apix/ControllerProduto.java
+++ src/main/java/com/github/phoa05/study_apix/ControllerProduto.java
@@ -8,27 +8,29 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
 @RequestMapping("/produtos")
 public class ControllerProduto {
 
     @PostMapping
-    public ResponseEntity<String> create(){
+    public ResponseEntity<String> create(@RequestBody Produto produto) {
         return ResponseEntity.status(201).body("Produto cadastrado");
     }
 
-    @PutMapping 
-    public ResponseEntity<String> update(){
+    @PutMapping
+    public ResponseEntity<String> update() {
         return ResponseEntity.status(200).body("produto atualizado");
     }
 
     @GetMapping
-    public ResponseEntity<String> find(){
-        return ResponseEntity.status(200).body( "banana");
+    public ResponseEntity<String> find() {
+        return ResponseEntity.status(200).body("banana");
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> delete(){
+    public ResponseEntity<Void> delete() {
         return ResponseEntity.status(204).build();
     }
 
